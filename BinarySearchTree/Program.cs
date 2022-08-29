c57fdef80885bec61b533ee590c58c3e7c745893
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-29 18:05:34 +0530

    [Darshan]: Add uc-2 to add nodes in binary search tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,10 +6,9 @@
             Console.WriteLine("Binary Search Tree BST!!!");
             BinaryTree<int> binary = new BinaryTree<int>();
 
-            Console.WriteLine("Enter 1 to Create Binary Search Tree");
-            Console.WriteLine("Enter 2 to Add elements in Binary Search Tree");
-            Console.WriteLine("Enter 3 to Search element in Binary Search Tree");
-
+            Console.WriteLine("Select choices from below---");
+            Console.WriteLine("1. To Create Binary Search Tree");
+            Console.WriteLine("2. To Add elements in Binary Search Tree");
             int Value = int.Parse(Console.ReadLine());
 
             switch (Value)
@@ -18,8 +17,27 @@
                     binary.AddNode(56);
                     binary.AddNode(30);
                     binary.AddNode(76);
+                   
+                    binary.CheckBST();
+                    break;
+
+                case 2:
+                    binary.AddNode(56);
+                    binary.AddNode(30);
+                    binary.AddNode(76);
+                    binary.AddNode(22);
+                    binary.AddNode(40);
+                    binary.AddNode(60);
+                    binary.AddNode(95);
+                    binary.AddNode(11);
+                    binary.AddNode(65);
+                    binary.AddNode(3);
+                    binary.AddNode(16);
+                    binary.AddNode(63);
+                    binary.AddNode(67);
 
                     binary.CheckBST();
+
                     break;
                 default:
                     Console.WriteLine("Enter valid input");
